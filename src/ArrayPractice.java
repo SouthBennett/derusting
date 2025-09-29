62552ef6d11103f60f9e1c31b38d95e96b2bccb9
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2025-09-29 14:13:59 -0700

    Added a print statement to the Array file

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,11 +1,17 @@
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
+    String words[] = new String[4];
 
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
+    words[0] = "Bike";
+    words[1] = "Car";
+    words[2] = "Boat";
+    words[3] = "Motorcycle";
 
     // Get the value of the array at index 2
+    System.out.println(words[1]);
 
     // Get the length of the array
 
