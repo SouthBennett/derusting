491ec7424677be4574c172656c527ecb6c1a5ded
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2025-09-30 10:16:19 -0700

    Created a new HashSet and assigned it to a variable of type set

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,9 +1,13 @@
+import java.util.HashSet
+
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
+    HashSet<String> words = new HashSet<>();
 
     // Add 3 elements to the set
     // (It's OK to do it one-by-one)
+    
 
     // Check whether the Set contains a given String
 
