8423fba5c5d8d18ad3964f5e08c2344581209b6e
SouthBennett <146032836+SouthBennett@users.noreply.github.com>
2025-09-29 22:02:47 -0700

    Used modulo to find the remainder when the int is divided by 3

--- src/NumberPractice.java
+++ src/NumberPractice.java
@@ -1,12 +1,17 @@
 public class NumberPractice {
   public static void main(String args[]) {
     // Create a float with a negative value and assign it to a variable
+    float negativeNum = -9.0f;
 
     // Create an int with a positive value and assign it to a variable
+    int positiveNum = 5;
 
     // Use the modulo % operator to find the remainder when the int is divided by 3
+    int remainder = positiveNum % 3;
+    System.out.println(positiveNum + " % 3 = " + remainder);
 
     // Use the modulo % operator to determine whether the number is even
+    
     // (A number is even if it has a remainder of zero when divided by 2)
     // Use an if-else to print "Even" if the number is even and "Odd"
     // if the number is odd.
